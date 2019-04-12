9e3d8738ae08b26e42c94a39d93930df016136de
Dherleth <erwan.moreira@gmail.com>
2019-04-12 10:32:30 +0200

    Classe Player : ajout d'un constructeur avec une piece, et d'un setter pour la piece

--- src/main/Player.java
+++ src/main/Player.java
@@ -2,8 +2,18 @@ package main;
 
 public class Player {
     private String name;
+    private Piece piece;
 
     public Player(String name) {
         this.name = name;
+    }
+
+    public Player(String name, Piece piece) {
+        this(name);
+        this.piece = piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
     }
 }
